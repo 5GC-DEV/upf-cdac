fd4683238ab009bc0fef7ac7082ea59a5a6fbaa1
BENCDAC <ben@cdac.in>
2025-05-16 13:36:26 +0530

    update and fix check

--- test/integration/framework.go
+++ test/integration/framework.go
@@ -4,7 +4,6 @@
 package integration
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"net"
@@ -246,8 +245,7 @@ func initForwardingPipelineConfig() {
 	}
 	defer providers.DisconnectP4rt()
 
-	ctx := context.Background()
-	_, err = p4rtClient.SetFwdPipe(ctx, deviceConfigPath, p4InfoPath, 0)
+	_, err = p4rtClient.SetFwdPipe(deviceConfigPath, p4InfoPath, 0)
 	if err != nil {
 		panic("Cannot init forwarding pipeline config: " + err.Error())
 	}
@@ -265,18 +263,17 @@ func mustInitCountersWithDummyValue() {
 
 	igCounterName := p4constants.GetCounterIDToNameMap()[p4constants.CounterPreQosPipePreQosCounter]
 	egCounterName := p4constants.GetCounterIDToNameMap()[p4constants.CounterPostQosPipePostQosCounter]
-	ctx := context.Background()
 	for i, maxSize := uint64(0), p4constants.CounterSizePreQosPipePreQosCounter; i < maxSize; i++ {
 		dummyValue := &v1.CounterData{
 			ByteCount:   1,
 			PacketCount: 1,
 		}
 
-		if err := p4rtClient.ModifyCounterEntry(ctx, igCounterName, int64(i), dummyValue); err != nil {
+		if err := p4rtClient.ModifyCounterEntry(igCounterName, int64(i), dummyValue); err != nil {
 			panic(err)
 		}
 
-		if err := p4rtClient.ModifyCounterEntry(ctx, egCounterName, int64(i), dummyValue); err != nil {
+		if err := p4rtClient.ModifyCounterEntry(egCounterName, int64(i), dummyValue); err != nil {
 			panic(err)
 		}
 	}
--- test/integration/providers/p4runtime.go
+++ test/integration/providers/p4runtime.go
@@ -36,9 +36,8 @@ func ConnectP4rt(addr string, asMaster bool) (*client.Client, error) {
 	}
 
 	c := p4_v1.NewP4RuntimeClient(grpcConn)
-	electionID := TimeBasedElectionId()
 	// Election only happens if asMaster is true.
-	p4RtC := client.NewClient(c, 1, &electionID, client.DisableCanonicalBytestrings)
+	p4RtC := client.NewClient(c, 1, TimeBasedElectionId(), client.DisableCanonicalBytestrings)
 
 	if asMaster {
 		// perform Master Arbitration
@@ -58,9 +57,8 @@ func ConnectP4rt(addr string, asMaster bool) (*client.Client, error) {
 		// deletes channel, otherwise DisconnectP4rt blocks forever for non-master P4runtime channel
 		stopCh = nil
 	}
-	ctx := context.Background()
 	// used to retrieve P4Info if exists on device
-	p4RtC.GetFwdPipe(ctx, client.GetFwdPipeP4InfoAndCookie)
+	p4RtC.GetFwdPipe(client.GetFwdPipeP4InfoAndCookie)
 
 	return p4RtC, nil
 }
