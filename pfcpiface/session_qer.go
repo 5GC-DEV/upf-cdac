b819e716ff9570a481b11b51adaa25d7fbc0c7bc
ReshmaJS <reshma.js@cdac.in>
2025-07-10 14:10:55 +0530

    logs with lowerbound checks

--- pfcpiface/session_qer.go
+++ pfcpiface/session_qer.go
@@ -69,8 +69,8 @@ func (s *PFCPSession) MarkSessionQer(qers []qer) {
 	sessQerIDList := make([]uint32, 0)
 	lastPdrIndex := len(s.pdrs) - 1
 	// create search list with first pdr's qerlist */
-	if lastPdrIndex >= len(s.pdrs) {
-		logger.PfcpLog.Warnf("=====MarkSessionQer: lastPdrIndex=%d out of  bounds s.pdrs len=%d", lastPdrIndex, len(s.pdrs))
+	if lastPdrIndex < 0 || lastPdrIndex >= len(s.pdrs) {
+		logger.PfcpLog.Warnf("=====MarkSessionQer: lastPdrIndex=%d out of bounds s.pdrs len=%d", lastPdrIndex, len(s.pdrs))
 		logger.PfcpLog.Infoln("=====return if lastPdrIndex out of bounds====")
 		return
 	}
