0ad29cc7580449fd638be7afd6c4e5f744b8d921
bhagathkrishnacdac <bhagath.krishna@cdac.in>
2026-05-06 19:47:05 +0530

    Update route_control.py

--- conf/route_control.py
+++ conf/route_control.py
@@ -16,7 +16,7 @@ from threading import Lock, Thread
 from typing import Dict, List, Optional, Tuple
 from pyroute2.netlink.rtnl.rtmsg import rtmsg
 from pyroute2.netlink.rtnl.ndmsg import ndmsg
-from pybess.bess import BESS
+from pybess.bess import *
 from pyroute2 import NDB, IPRoute
 from scapy.all import ICMP, IP, send
 from socket import AF_INET
